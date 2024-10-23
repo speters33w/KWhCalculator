591c2511929e3d6395d86a0f7d47239643e0e76d
speters33w <speters33w@gmail.com>
2024-10-22 20:19:20 -0400

    Added appliances to js

--- KWhCalc.js
+++ KWhCalc.js
@@ -7,11 +7,28 @@ function createDropdown(selectElement) {
         const newDropdown = document.createElement('div');
         newDropdown.className = 'dropdown';
         newDropdown.innerHTML = `
-                    <select id="appliance${dropdownCount}" onchange="createDropdown(this)">
-                        <option value="">Select</option>
-                        <option value="refrigerator">Refrigerator</option>
-                        <option value="washer">Washer</option>
-                        <option value="toaster">Toaster</option>
+             <select id=appliance1" onchange="createDropdown(this)">
+                        <option value=0>Select</option>
+                        <option value=1.5>Portable heater (1500W)</option>
+                        <option value=0.73>Air Conditioner (Window/wall) (8kBTU) (120V)</option>
+                        <option value=1.5>Water Heater (Tank Type) (Estimate)</option>
+                        <option value=0.17>Water Heater Instantaneous (110 v 29 amp @1gpm 70°F)</option>
+                        <option value=2.3>Oven</option>
+                        <option value=1.5>Surface Oven</option>
+                        <option value=1.5>Microwave Oven</option>
+                        <option value=0.4>Coffee Maker</option>
+                        <option value=1.5>Dishwasher (Does not include water heater)</option>
+                        <option value=0.4>Toaster</option>
+                        <option value=0.75>Toaster Oven</option>
+                        <option value=0.17>Refrigerator, 15 cu. ft.</option>
+                        <option value=0.02>Freezer (if not opened)</option>
+                        <option value=0.071>Television (40"–50" LED/4k UHD)</option>
+                        <option value=0.019>Television (30"–40"  LED/HDTV/1080P)</option>
+                        <option value=0.15>Desktop Computer (150 Watt)</option>
+                        <option value=0.04>Monitor (17" LCD)</option>
+                        <option value=0.04>Laptop Computer</option>
+                        <option value=0.04>LED bulb (20 W (75 W incandescent equivalent per bulb) </option>
+                        <option value=2.3>Washing Machine (warm wash, cold rinse)</option>
                     </select>
                     
                     <label for="hours$">Hours:</label>
